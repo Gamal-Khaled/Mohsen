c55248a1b371d489047d9590a5d205927752969a
Gamal Khaled <gamal.khaled11@gmail.com>
2020-03-11 19:42:41 +0200

    adding STT service

--- App.tsx
+++ App.tsx
@@ -1,118 +1,105 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, { Component } from 'react';
+import { StyleSheet, Button, View, Text, FlatList } from 'react-native';
+import { SpeechResultsEvent } from '@react-native-community/voice';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import SpeechToTextService from './src/services/SpeechToTextService';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+interface Props { }
+interface State {
+    recordingState: string,
+    text: string[],
+    workingText: string[],
+}
 
-declare var global: {HermesInternal: null | {}};
+class App extends Component<Props, State> {
+    state = {
+        recordingState: "not recording",
+        text: [],
+        workingText: [],
+    }
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+    constructor(props: Props) {
+        super(props);
+
+        SpeechToTextService.initialize({
+            onSpeechStartHandler: this.onSpeechStartHandler.bind(this),
+            onSpeechEndHandler: this.onSpeechEndHandler.bind(this),
+            onSpeechResultsHandler: this.onSpeechResultsHandler.bind(this),
+            onSpeechPartialResultsHandler: this.onSpeechPartialResultsHandler.bind(this),
+            onSpeechVolumeChangedHandler: this.onSpeechVolumeChangedHandler.bind(this),
+        })
+    }
+
+    onSpeechVolumeChangedHandler = () => null
+
+    onSpeechStartHandler = (p: any) => {
+        this.setState({ recordingState: "recording" });
+    }
+
+    onSpeechEndHandler = (p: any) => {
+        this.setState({ recordingState: "finished recording" });
+    }
+
+    onSpeechResultsHandler = (text: SpeechResultsEvent) => {
+        this.setState({ recordingState: "result done", text: text.value || [] });
+        console.log(text)
+    }
+
+    onSpeechPartialResultsHandler = (event: SpeechResultsEvent) => {
+        this.setState({ workingText: event.value || [] });
+    }
+
+    onStartButtonPress() {
+        SpeechToTextService.start('en-US');
+    }
+
+    render() {
+        return (
+            <View>
+                <View style={styles.recordingStateContainer}>
+                    <View style={[styles.recordingState, { backgroundColor: this.state.recordingState === "recording" ? "green" : "red" }]} />
+                    <Text>{this.state.recordingState}</Text>
+                </View>
+                <Button
+                    title="Record"
+                    onPress={this.onStartButtonPress}
+                />
+                <View style={styles.Divider} />
+                <Button
+                    title="Stop Recording (Shut up to stop automatically)"
+                    onPress={SpeechToTextService.stop}
+                />
+                <View style={styles.Divider} />
+                <Text style={styles.working}>{this.state.workingText[this.state.workingText.length - 1]}</Text>
+                <View style={styles.Divider} />
+                <FlatList
+                    data={this.state.text}
+                    renderItem={({ item }) => <Text>{item}</Text>}
+                    keyExtractor={item => item}
+                />
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+        )
+    }
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    Divider: {
+        height: 1,
+        backgroundColor: "#000",
+    },
+    working: {
+        color: "green"
+    },
+    recordingStateContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    recordingState: {
+        height: 10,
+        width: 10,
+        borderRadius: 5,
+        marginHorizontal: 5,
+    },
 });
 
 export default App;
