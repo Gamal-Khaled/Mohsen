f9a22ba0c404daef6a31ffa90a2d7670d6ba2c9d
Hossam Mohamed <31036061+Hossam777@users.noreply.github.com>
2020-06-01 17:18:01 +0200

    Adding Service

--- App.tsx
+++ App.tsx
@@ -20,23 +20,37 @@ import snowboy from "react-native-snowboy";
 
 const App = () => {
   //Call the function here
+  snowboy.addEventListener("msg-active", (e) => {
+		console.log("hehehe")
+	})
  let click = function(){
-   snowboy.initHotword()
+  snowboy.initHotword()
 	.then((res)=> {
 		console.log(res)
-    snowboy.start();
+    snowboy.startRecording();
 	})
 	.catch((err)=> {
 		console.log(err)
 	})
+  
+ };
+ let click2 = function(){
+  snowboy.startService();
+ };
+ let click3 = function(){
+  snowboy.stopService();
  };
   
   return (
     <View>
       <Text>Hi</Text>
       <Button
-          title="Press me"
-          onPress={() => click()}
+          title="start"
+          onPress={() => click2()}
+        />
+        <Button
+          title="stop"
+          onPress={() => click3()}
         />
     </View>
   );
